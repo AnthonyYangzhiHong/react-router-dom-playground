39f4e6772840cc1b488b0da90fc4e560eac9fc3d
AnthonyYangzhiHong <yangzhihong90@hotmail.com>
2019-04-26 09:05:26 -0400

    Migration prep

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+//import AppRouter from './AppRouter.js';
+//import ParamsExample from './ParamsExample.js';
+import AuthExample from './AuthExample.js';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AuthExample />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
